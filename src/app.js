a70a27ed3158aacc89caec14d08970f17a6271d4
Asun Perea Ferrer <118168204+asun-pf@users.noreply.github.com>
2023-05-19 19:16:32 +0000

    added the button

--- src/app.js
+++ src/app.js
@@ -2,32 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
   // document.getElementById("#the-excuse").textContent = generateExcuse();
 
   document.querySelector("#the-excuse").innerHTML = generateExcuse();
 };
 
-let generateExcuse = () => {
-  let subject = [
+const generateExcuse = () => {
+  const subject = [
     "A jogger",
     "A racoon",
     "My neighbor",
     "My cat",
     "My pinecone"
   ];
-  let actions = ["threw m", "ate m", "lost my", "burnt", "chew "];
-  let possesion = [
+  const actions = ["threw m", "ate m", "lost my", "burnt", "chew "];
+  const possesion = [
     "my homework",
     "my flowers",
     "my car",
     "my cat",
     "my pinecone"
   ];
-  let where = [
+  const where = [
     "in the street",
     "in my pool",
     "in my kitchen",
@@ -35,10 +32,10 @@ let generateExcuse = () => {
     "in my bathroom"
   ];
 
-  let subjectIndex = Math.floor(Math.random() * subject.length);
-  let actionsIndex = Math.floor(Math.random() * actions.length);
-  let possesionIndex = Math.floor(Math.random() * possesion.length);
-  let whereIndex = Math.floor(Math.random() * where.length);
+  const subjectIndex = Math.floor(Math.random() * subject.length);
+  const actionsIndex = Math.floor(Math.random() * actions.length);
+  const possesionIndex = Math.floor(Math.random() * possesion.length);
+  const whereIndex = Math.floor(Math.random() * where.length);
 
   return (
     subject[subjectIndex] +
