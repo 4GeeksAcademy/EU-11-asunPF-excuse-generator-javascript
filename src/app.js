aa5ccac0f4dee7d9f0ba3bdefbc09dd2c5774791
Asun Perea Ferrer <118168204+asun-pf@users.noreply.github.com>
2023-05-30 20:58:36 +0000

    used the querySelector, turned my uxcuses into an object inside an arrow, created an empty variable for my final result. Took the values of the arrays in my object with Object.value one by one with the for.Each method. and created a constant to randomize this values

--- src/app.js
+++ src/app.js
@@ -3,35 +3,45 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = () => {
-  // document.getElementById("#the-excuse").textContent = generateExcuse();
-
   document.querySelector("#the-excuse").innerHTML = generateExcuse();
 };
 
 const generateExcuse = () => {
-  const subject = [
-    "A jogger",
-    "A racoon",
-    "My neighbor",
-    "My cat",
-    "My pinecone"
-  ];
-  const actions = ["threw m", "ate m", "lost my", "burnt", "chew "];
-  const possesion = [
-    "my homework",
-    "my flowers",
-    "my car",
-    "my cat",
-    "my pinecone"
-  ];
-  const where = [
-    "in the street",
-    "in my pool",
-    "in my kitchen",
-    "in my oven",
-    "in my bathroom"
-  ];
+  const excuseObject = {
+    subject: ["A jogger", "A racoon", "My neighbor", "My cat", "My pinecone"],
+    actions: ["threw", "ate", "lost", "burnt", "chewed"],
+    possession: [
+      "my homework",
+      "my flowers",
+      "my car",
+      "my cat",
+      "my pinecone"
+    ],
+    where: [
+      "in the street",
+      "in my pool",
+      "in my kitchen",
+      "in my oven",
+      "in my bathroom"
+    ]
+  };
 
+  let finalPhrase = "";
+
+  Object.values(excuseObject).forEach(arr => {
+    const randomizingTheExcuseObjectValues = Math.floor(
+      Math.random() * arr.length
+    );
+    finalPhrase += arr[randomizingTheExcuseObjectValues] + " ";
+  });
+
+  return finalPhrase;
+};
+
+/* 
+  This is what I had before... just trying a simpler form above
+  
+  
   const subjectIndex = Math.floor(Math.random() * subject.length);
   const actionsIndex = Math.floor(Math.random() * actions.length);
   const possesionIndex = Math.floor(Math.random() * possesion.length);
@@ -47,3 +57,4 @@ const generateExcuse = () => {
     where[whereIndex]
   );
 };
+*/
